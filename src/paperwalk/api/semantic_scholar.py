ee0ae342fcfd59dac44115dde4da7ae9f1a638ca
ZincCat <zincchloride@outlook.com>
2024-01-13 18:30:11 -0500

    more fields

--- src/paperwalk/api/semantic_scholar.py
+++ src/paperwalk/api/semantic_scholar.py
@@ -5,8 +5,26 @@ class SemanticScholarAPI:
     def __init__(self):
         pass
 
-    def fetch_data_from_api(self, paper_id):
-        url = f"https://api.semanticscholar.org/graph/v1/paper/{paper_id}/citations?fields=title,citationCount,externalIds,abstract"
+    def fetch_paper(self, paper_id):
+        url = f"https://api.semanticscholar.org/graph/v1/paper/{paper_id}?fields=title,authors,abstract,citationCount,referenceCount,externalIds,year"
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print("Failed to fetch data")
+            return None
+
+    def fetch_citations(self, paper_id):
+        url = f"https://api.semanticscholar.org/graph/v1/paper/{paper_id}/citations?fields=title,authors,abstract,citationCount,referenceCount,externalIds,year"
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print("Failed to fetch data")
+            return None
+        
+    def fetch_references(self, paper_id):
+        url = f"https://api.semanticscholar.org/graph/v1/paper/{paper_id}/references?fields=title,authors,abstract,citationCount,referenceCount,externalIds,year"
         response = requests.get(url)
         if response.status_code == 200:
             return response.json()
--- src/paperwalk/database/database.py
+++ src/paperwalk/database/database.py
@@ -44,21 +44,56 @@ class Neo4jConnection:
             if session is not None:
                 session.close()
         return response
-
+    
 def insert_paper_data(conn, paper_id, paper_data):
-    for citing_paper in paper_data['data']:
+    query = '''
+    MERGE (p:Paper {paperId: $paperId})
+    ON CREATE SET p.title = $title, p.firstAuthor = $firstAuthor, \
+        p.firstAuthorId = $firstAuthorId, p.lastAuthor = $lastAuthor, \
+        p.lastAuthorId = $lastAuthorId, p.abstract = $abstract, \
+        p.citationCount = $citationCount, p.referenceCount = $referenceCount, \
+        p.externalIds = $externalIds, p.year = $year
+    '''
+    parameters = {
+        'paperId': paper_id,
+        'title': paper_data['title'],
+        'firstAuthor': paper_data['authors'][0]['name'],
+        'firstAuthorId': paper_data['authors'][0]['authorId'],
+        'lastAuthor': paper_data['authors'][-1]['name'],
+        'lastAuthorId': paper_data['authors'][-1]['authorId'],
+        'abstract': paper_data['abstract'],
+        'citationCount': int(paper_data['citationCount']) if paper_data['citationCount'] else 0,
+        'referenceCount': int(paper_data['referenceCount']) if paper_data['referenceCount'] else 0,
+        'externalIds': paper_data['externalIds']['Arxiv'] if 'Arxiv' in paper_data['externalIds'] else None,
+        'year': paper_data['year']
+    }
+    conn.query(query, parameters)
+
+def insert_citation_data(conn, paper_id, citation_data):
+    for citing_paper in citation_data['data']:
         query = '''
         MERGE (p1:Paper {paperId: $paperId})
         MERGE (p2:Paper {paperId: $citingPaperId})
-        ON CREATE SET p2.title = $title, p2.abstract = $abstract, p2.citationCount = $citationCount
+        ON CREATE SET p2.title = $title, p2.firstAuthor = $firstAuthor, \
+            p2.firstAuthorId = $firstAuthorId, p2.lastAuthor = $lastAuthor, \
+            p2.lastAuthorId = $lastAuthorId, p2.abstract = $abstract, \
+            p2.citationCount = $citationCount, p2.referenceCount = $referenceCount, \
+            p2.externalIds = $externalIds, p2.year = $year
         MERGE (p2)-[:CITES]->(p1)
         '''
         parameters = {
             'paperId': paper_id,
             'citingPaperId': citing_paper['citingPaper']['paperId'],
             'title': citing_paper['citingPaper']['title'],
+            'firstAuthor': citing_paper['citingPaper']['authors'][0]['name'],
+            'firstAuthorId': citing_paper['citingPaper']['authors'][0]['authorId'],
+            'lastAuthor': citing_paper['citingPaper']['authors'][-1]['name'],
+            'lastAuthorId': citing_paper['citingPaper']['authors'][-1]['authorId'],
             'abstract': citing_paper['citingPaper']['abstract'],
-            'citationCount': int(citing_paper['citingPaper']['citationCount']) if citing_paper['citingPaper']['citationCount'] else 0
+            'citationCount': int(citing_paper['citingPaper']['citationCount']) if citing_paper['citingPaper']['citationCount'] else 0,
+            'referenceCount': int(citing_paper['citingPaper']['referenceCount']) if citing_paper['citingPaper']['referenceCount'] else 0,
+            'externalIds': citing_paper['citingPaper']['externalIds']['Arxiv'] if 'Arxiv' in citing_paper['citingPaper']['externalIds'] else None,
+            'year': citing_paper['citingPaper']['year']
         }
         conn.query(query, parameters)
 
@@ -72,10 +107,13 @@ if __name__ == "__main__":
 
     # Replace with the actual paper ID
     paper_id = "649def34f8be52c8b66281af98ae884c09aef38b"
-    paper_data = semantic_scholar_api.fetch_data_from_api(paper_id)
-
+    paper_data = semantic_scholar_api.fetch_paper(paper_id)
     if paper_data:
         insert_paper_data(conn, paper_id, paper_data)
+
+    citation_data = semantic_scholar_api.fetch_citations(paper_id)
+    if citation_data:
+        insert_citation_data(conn, paper_id, citation_data)
 
     # query
     query = '''
