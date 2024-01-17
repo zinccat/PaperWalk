feff85593ca9f9468c38a1a02d2cae616413b405
ZincCat <zincchloride@outlook.com>
2024-01-16 23:09:29 -0500

    organized fields

--- src/paperwalk/api/semantic_scholar.py
+++ src/paperwalk/api/semantic_scholar.py
@@ -10,30 +10,30 @@ class SemanticScholarAPI:
             }
         else:
             self.header = None
+        self.timeout = 3
+        self.fields = "title,authors,abstract,citationCount,referenceCount,externalIds,year"
+        self.limit = 10
+
+    def _request(self, url):
+        try:
+            response = requests.get(url, headers=self.header, timeout=self.timeout)
+            if response.status_code == 200:
+                return response.json()
+            else:
+                print("Failed to fetch data")
+                return None
+        except Exception as e:
+            print("Failed to fetch data:", e)
+            return None
 
     def fetch_paper(self, paper_id):
-        url = f"https://api.semanticscholar.org/graph/v1/paper/{paper_id}?fields=title,authors,abstract,citationCount,referenceCount,externalIds,year&limit=10"
-        response = requests.get(url)
-        if response.status_code == 200:
-            return response.json()
-        else:
-            print("Failed to fetch data")
-            return None
-
+        url = f"https://api.semanticscholar.org/graph/v1/paper/{paper_id}?fields={self.fields}&limit={self.limit}"
+        return self._request(url)
+    
     def fetch_citations(self, paper_id):
-        url = f"https://api.semanticscholar.org/graph/v1/paper/{paper_id}/citations?fields=title,authors,abstract,citationCount,referenceCount,externalIds,year&limit=10"
-        response = requests.get(url)
-        if response.status_code == 200:
-            return response.json()
-        else:
-            print("Failed to fetch data")
-            return None
+        url = f"https://api.semanticscholar.org/graph/v1/paper/{paper_id}/citations?fields={self.fields}&limit={self.limit}"
+        return self._request(url)
         
     def fetch_references(self, paper_id):
-        url = f"https://api.semanticscholar.org/graph/v1/paper/{paper_id}/references?fields=title,authors,abstract,citationCount,referenceCount,externalIds,year&limit=10"
-        response = requests.get(url)
-        if response.status_code == 200:
-            return response.json()
-        else:
-            print("Failed to fetch data")
-            return None
+        url = f"https://api.semanticscholar.org/graph/v1/paper/{paper_id}/references?fields={self.fields}&limit={self.limit}"
+        return self._request(url)
